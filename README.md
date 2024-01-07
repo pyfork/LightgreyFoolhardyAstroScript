12ac24c3417dc4c3f739e2953283a320b8dd7868
pyfork <118096225+pyfork@users.noreply.github.com>
2024-01-07 08:25:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hand Made Heiken Ashi Scanner Script
+
+By Ahmad
+
+## File format
+
+Saved as `js` as Pinescript's syntax is close to JS. Pinescript is not JS.
---
+++ scanner.js
@@ -0,0 +1,50 @@
+//@version=5
+// indicator("AMD 5HA Scanner", overlay=true)
+indicator("AMD 5HA Scanner", overlay=true, max_bars_back=150)
+
+// User-defined input for shape and text colors
+color bullishShapeColor = input(color.new(#c8e6c9, 0), "Bullish Shape Color")
+color bearishShapeColor = input(color.new(#fccbcd, 0), "Bearish Shape Color")
+color bullishTextColor = input(color.new(#4caf50, 0), "Bullish Text Color")
+color bearishTextColor = input(color.new(#f77c80, 0), "Bearish Text Color")
+
+var float haOpen = na  // Declare haOpen with an initial value
+var float haClose = na  // Declare haClose with an initial value
+
+// Heiken Ashi calculations
+haClose := (open + high + low + close) / 4
+haOpen := na(haOpen[1]) ? (open + close) / 2 : (haOpen[1] + haClose[1]) / 2
+
+// Determine type of HA candles: 1 for bullish, -1 for bearish
+var int haType = 0 // Initializing haType
+haType := haClose > haOpen ? 1 : haClose < haOpen ? -1 : nz(haType[1], 0)
+
+// Count consecutive candles of the same type
+var int consecCandles = 0
+consecCandles := haType == haType[1] ? consecCandles + 1 : 1
+
+// Store the close of the first candle in the sequence
+var float firstClose = na
+firstClose := consecCandles == 1 ? haClose : firstClose
+
+// Reset the first close after 5 candles to avoid referencing too far back in history
+if consecCandles > 5
+    firstClose := na
+
+// Calculate total percent change over 5 consecutive candles of the same type
+var float percentChange = 0.0
+if consecCandles >= 5
+    percentChange := 100 * (haClose - firstClose) / firstClose
+percentChange := math.abs(percentChange)
+
+// Condition for 5 consecutive candles of the same type with a total percent change greater than 0.05%
+longCondition = consecCandles >= 5 and haType == 1 and percentChange > 0.05
+shortCondition = consecCandles >= 5 and haType == -1 and percentChange > 0.05
+
+// Plotting the signal with user-defined color for shape and text
+plotshape(series=longCondition, location=location.belowbar, color=bullishShapeColor, style=shape.labelup, textcolor=bullishTextColor, text="Bullish 5HA")
+plotshape(series=shortCondition, location=location.abovebar, color=bearishShapeColor, style=shape.labeldown, textcolor=bearishTextColor, text="Bearish 5HA")
+
+// Optional: Plotting Heiken Ashi values for reference
+plot(haClose, "HA Close", color=color.blue)
+plot(haOpen, "HA Open", color=color.orange)
